9d63284ccd21ee1d60ce84c3a4404308c7a5e1d2
James White <whitejaad@gmail.com>
2022-09-07 10:48:53 -0700

    Initial commit

---
+++ spec/employee_spec.rb
@@ -0,0 +1,21 @@
+require'pry'
+require'./lib/employee'
+
+RSpec.describe Employee do
+  before do
+  @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+  end
+
+  it'exist'do
+  expect(@bobbi).to be_an_instance_of(Employee)
+  end
+
+  it'has readable info'do
+  expect(@bobbi.name).to eq("Bobbi Jaeger")
+  expect(@bobbi.age).to eq("30")
+  expect(@bobbi.salary).to eq("100000")
+
+  end
+
+
+end
